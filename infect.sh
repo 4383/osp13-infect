b44c87e2198cc575319592cb36f3e894552410f1
Hervé Beraud <hberaud@redhat.com>
2019-04-18 17:28:17 +0200

    remove python and change repo url

--- infect.sh
+++ infect.sh
@@ -4,7 +4,5 @@ SSH="ssh -F $SSHCONF undercloud"
 SCP="scp -F $SSHCONF"
 
 ${SSH} 'yum install -y vim git gdb'
-${SSH} 'python -m ensurepip'
-${SSH} 'python -m pip install niet'
-${SSH} 'su - stack sh -l -c "git clone https://gitlab.cee.redhat.com/hberaud/osp13-infect.git"'
+${SSH} 'su - stack sh -l -c "git clone https://github.com/4383/osp13-infect.git"'
 ${SSH} 'su - stack sh -l -c "cd osp13-infect/engine && ./undercloud.sh"'
