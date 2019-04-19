e710c6f340105587b4876001f6c4d095ada42d99
Hervé Beraud <hberaud@redhat.com>
2019-04-19 16:02:28 +0200

    centralize into infect-payload.sh

--- engine/undercloud.sh
+++ engine/undercloud.sh
@@ -6,12 +6,6 @@ cd /home/stack
 echo ${BASEDIR}
 source stackrc;
 
-cat <<'EOF' >>~/.bashrc
-# history navigation with arrows
-bind '"\e[A":history-search-backward'
-bind '"\e[B":history-search-forward'
-source /usr/bin/infect-payload.sh
-EOF
-
+cat ${BASEDIR}/bashrc >> ~/.bashrc
 
 ansible-playbook ${BASEDIR}/playbooks/rhel-repos.yaml -vvv
