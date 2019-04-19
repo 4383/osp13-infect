8bc94bf40512c72e9c651594a78876610f20de46
Hervé Beraud <hberaud@redhat.com>
2019-04-19 11:58:24 +0200

    fix docker utilities

--- engine/containers.sh
+++ engine/containers.sh
@@ -11,8 +11,6 @@ function find_oslo () {
     name=$1
     for container in $(list_containers)
     do
-        docker exec \
-            -it ${container} \
-            'test -d /usr/lib/python2.7/site-packages/oslo_${name} && echo ${container}'
+        docker exec -it ${container} test -d /usr/lib/python2.7/site-packages/oslo_${name} && echo ${container}
     done
 }
