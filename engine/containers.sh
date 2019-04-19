630d5392ec20e196e4f58e51208f23d2f236b6c1
Hervé Beraud <hberaud@redhat.com>
2019-04-19 12:16:16 +0200

    improve docker utilities

--- engine/containers.sh
+++ engine/containers.sh
@@ -2,7 +2,7 @@ function list_containers () {
     docker ps --format "{{.Names}}"
 }
 
-function find_oslo () {
+function who_use_oslo () {
     if [ $# -eq 0 ] 
     then
         echo "Please provide oslo project name"
@@ -13,4 +13,39 @@ function find_oslo () {
     do
         docker exec -it ${container} test -d /usr/lib/python2.7/site-packages/oslo_${name} && echo ${container}
     done
+}
+
+function who_use_this_python_module () {
+    if [ $# -eq 0 ] 
+    then
+        echo "Please provide module name"
+        return 1
+    fi
+    name=$1
+    for container in $(list_containers)
+    do
+        docker exec -it ${container} test -d /usr/lib/python2.7/site-packages/${name} && echo ${container}
+    done
+}
+
+function all_packages_versions () {
+    if [ $# -eq 0 ] 
+    then
+        echo "Please provide a container name"
+        return 1
+    fi
+    name=$1
+    docker exec -it ${name} rpm -qa
+}
+
+function given_packages_versions () {
+    if [ $# -lt 2 ] 
+    then
+        echo "Please provide a container name and package name"
+        echo "usage: given_packages_versions nova_api oslo_messaging|amqp"
+        return 1
+    fi
+    name=$1
+    packages=$2
+    docker exec -it ${name} rpm -qa | grep -e "${packages}"
 }
