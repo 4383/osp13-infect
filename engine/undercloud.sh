27e76a481118e367c46c3fd126c849ab768cd8f5
Hervé Beraud <hberaud@redhat.com>
2019-04-24 11:36:30 +0200

    fix infect

--- engine/undercloud.sh
+++ engine/undercloud.sh
@@ -5,7 +5,9 @@ BASEDIR=$(pwd)
 cd /home/stack
 echo ${BASEDIR}
 source stackrc;
+source /usr/bin/infect-payload.sh
 
 cat ${BASEDIR}/bashrc >> ~/.bashrc
 
 ansible-playbook ${BASEDIR}/playbooks/rhel-repos.yaml
+infect_conf_backup
